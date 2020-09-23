25300ce336ef2b0dcf1b17366e035a239b94a5b8
Zakari Nazim SAIBI <saibi.nazim@gmail.com>
2020-09-23 22:58:34 +0200

    added styling

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "../utils/axios";
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -19,11 +20,15 @@ const Row = (props) => {
   console.log(movies);
 
   return (
-    <div className="rom">
+    <div className="row">
       <h2>{props.title}</h2>
       <div className="row__posters">
         {movies.map((movie) => (
-          <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
         ))}
       </div>
     </div>
