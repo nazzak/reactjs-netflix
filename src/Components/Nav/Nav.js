318a0945011a2f67679ba0a9f266af762afae4ee
Zakari Nazim SAIBI <saibi.nazim@gmail.com>
2020-09-24 16:37:26 +0200

    added a listener to the scroll Y with a transition

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
 const Nav = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    // add a listener to the scroll in Y
+    // display the background if scroll in Y > 100 px
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    // return a cleanup
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <img
         className="nav__logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
