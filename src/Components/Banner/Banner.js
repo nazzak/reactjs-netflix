2c3f1e02a776538aa66e77406c959dbbbd94befc
Zakari Nazim SAIBI <saibi.nazim@gmail.com>
2020-09-24 09:42:49 +0200

    done with the banner

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -23,6 +23,11 @@ const Banner = () => {
 
   //console.log(movie);
 
+  // truncate a string and replace with 3 dots
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
+
   return (
     <header
       className="banner"
@@ -33,12 +38,18 @@ const Banner = () => {
       }}
     >
       <div className="banner__contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
-        <button className="banner__button">Play</button>
-        <button className="banner__button">My List</button>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 class="banner__description">{truncate(movie?.overview, 150)}</h1>
       </div>
 
-      <h1 class="banner__description">{movie?.overview}</h1>
+      {/* Added for styling the bottom for the banner */}
+      <div className="banner--fadeBottom"></div>
     </header>
   );
 };
