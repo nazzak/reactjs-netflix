a0ffad545ce7461150769112ee11023c0fe75534
Zakari Nazim SAIBI <saibi.nazim@gmail.com>
2020-09-24 10:04:04 +0200

    added netflix logo + styling

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "./Nav.css";
 
 const Nav = () => {
   return (
     <div className="nav">
-      <h1>Netflix</h1>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
     </div>
   );
 };
